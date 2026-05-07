14c0f1bcf980283f59bc52843e2a64afe0a6bbd9
Monmayuri Gogoi <monmayurigogoi84@gmail.com>
2026-05-07 23:58:52 +0530

    fixed railway deployment

--- server.js
+++ server.js
@@ -2,22 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Server is running!');
+    res.send('Server is running');
 });
 
-app.post('/user', (req, res) => {
-
-    const userData = req.body;
-
-    res.send({
-        message: 'Data received successfully',
-        data: userData
-    });
-});
-
-app.listen(process.env.PORT || 8080, '0.0.0.0', () => {
-    console.log('Server is running');
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
