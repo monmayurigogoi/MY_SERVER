631c24753a55b63e51f0febbc495933f50aa5582
Monmayuri Gogoi <monmayurigogoi84@gmail.com>
2026-05-28 15:24:32 +0530

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,48 @@
-const express = require('express');
+const http = require("http");
 
-const app = express();
+const server = http.createServer((req, res) => {
 
-const PORT = process.env.PORT || 3000;
+    // GET REQUEST
+    if(req.method === "GET"){
 
-app.get('/', (req, res) => {
-    res.send('Server is running');
+        res.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+
+        res.end(JSON.stringify({
+            message: "GET Request Working"
+        }));
+
+    }
+
+    // POST REQUEST
+    else if(req.method === "POST"){
+
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+
+        req.on("end", () => {
+
+            const data = JSON.parse(body);
+
+            res.writeHead(200, {
+                "Content-Type": "application/json"
+            });
+
+            res.end(JSON.stringify({
+                message: "POST Request Working",
+                receivedData: data
+            }));
+
+        });
+
+    }
+
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
